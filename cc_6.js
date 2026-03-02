5c3035a55e181ce9e575707b0f8f44b67eb2745d
arianaramirezusf <arianaramirez@usf.edu>
2026-03-01 22:51:47 -0500

    Creating base class

---
+++ cc_6.js
@@ -0,0 +1,15 @@
+//Creating Base Class
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+};
+
+describe() {
+    return `Employee: ${this.name} | Department: ${this.department}`;
+};
+
+
+//Creating Subclass
+
